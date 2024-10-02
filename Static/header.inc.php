0d4e49becf612f13b243bc267b8d77f09d8057b5
alim26 <andrewlimbachtiar@gmail.com>
2024-10-02 16:11:40 -0700

    Commiting Changes

---
+++ Static/header.inc.php
@@ -0,0 +1,37 @@
+<!-- 
+    Programmer: Andrew Lim
+    Date Created: 06/23/2024
+    Date Last Modified: 06/24/2024
+-->
+
+<?php
+
+?>
+
+<head>
+    <script src="https://kit.fontawesome.com/224258cc50.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../styling/default_base.css">
+</head>
+
+<!-- This is a common navigation base to call different web pages on the website -->
+<div class="mainHeader">
+    <div class="headerLeft">
+        <div class="webPageLogo">
+            <h1>AboutDrew</h1>
+        </div>
+
+        <div class="webPageTitle">
+            <p>PERSONAL<br>
+               PORTFOLIO
+            </p>
+        </div>
+    </div>
+
+    <div class="headerRight">
+        <a href="home_page.php">Home</a>
+        <a href="">Education</a>
+        <a href="">Skills</a>
+        <a href="">Projects</a>
+        <a href="">Contact</a>
+    </div>
+</div>
